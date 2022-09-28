5b44fa03a9ce9bc21c737fb06ec4ab9f9b771e0a
bohdandrahan <bohdan.drahan@gmail.com>
2022-09-27 22:10:12 -0400

    solve 6.2

--- 2021/6/fish_calculator.py
+++ 2021/6/fish_calculator.py
@@ -1,8 +1,8 @@
 
 class Fish_Calculator():
-    def __init__(self, data_list):
+    def __init__(self, data_list, days):
         self.data = data_list;
-        self.num_of_gens = 256;
+        self.num_of_gens = days;
 
     def run_one_gen(self, pop):
         next_gen = []
@@ -18,5 +18,40 @@ class Fish_Calculator():
         for i in range(self.num_of_gens):
             print("gen:", i)
             self.population = self.run_one_gen(self.population);
+        print("days:", self.num_of_gens, "naive method: pop: ", len(self.population))
 
-        print("pop: ", len(self.population))
+    def format_dict(self):
+        self.dict = {}
+        for each in self.data:
+            if each in self.dict:
+                self.dict[each] += 1
+            else:
+                self.dict[each] = 1
+        for each in range(9):
+            if not each in self.dict:
+                self.dict[each] = 0
+
+    def run_one_gen_dict(self, pop):
+        next_gen = {}
+        for i in range(9):
+            if i == 0:
+                pop[9] = pop[0]
+                if 7 in pop:
+                    pop[7] += pop[0]
+
+            if i + 1 in pop:
+                pop[i] = pop[i+1]
+        return pop
+
+    def run_all_gens_dict(self):
+        self.format_dict()
+        self.population = self.dict
+
+        for i in range(self.num_of_gens):
+            self.population = self.run_one_gen_dict(self.population)
+
+        total = 0
+        for each in self.population:
+            if not each == 9:
+                total += self.population[each]
+        print("days:", self.num_of_gens, "not naive method, pop:" ,total)
--- 2021/6/main.py
+++ 2021/6/main.py
@@ -4,5 +4,9 @@ from fish_calculator import Fish_Calculator;
 file = Input_Reader('input.txt')
 print("input: ", file.get_list())
 
-calculator = Fish_Calculator(file.get_list())
+calculator = Fish_Calculator(file.get_list(), 80)
+
 calculator.run_all_gens()
+calculator = Fish_Calculator(file.get_list(), 256)
+calculator.format_dict()
+calculator.run_all_gens_dict()
