834e658fdc5d131de8219242cfed475a5e296c17
bohdandrahan <bohdan.drahan@gmail.com>
2024-01-26 14:43:56 -0500

    solve part 1 2023/6

--- 2023/6/calculator.py
+++ 2023/6/calculator.py
@@ -5,7 +5,25 @@ class Calculator():
         self.distances = data[1]
 
     def calculate1(self):
-        pass
+
+        result = 1
+        
+        for i in range(len(self.times)):
+            num_of_beatings = 0
+
+            time = self.times[i]
+            distance = self.distances[i]
+
+            for speed in range(time): #speed will be equal to the number of seconds we wait
+                range_of_boat = speed * (time-speed)
+                if range_of_boat > distance:
+                    num_of_beatings += 1
+            
+            result *= num_of_beatings
+        
+        return result
+
+
     
     def calculate2(self):
         pass
