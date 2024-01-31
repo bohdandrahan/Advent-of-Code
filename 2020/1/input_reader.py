7b8eb1c2991345406231f1ac7ff37ff388c8ddbc
bohdandrahan <bohdan.drahan@gmail.com>
2024-01-30 22:50:57 -0500

    init 2020/1

---
+++ 2020/1/input_reader.py
@@ -0,0 +1,14 @@
+class Input_Reader():
+    def __init__(self,file_path):
+        self.file = open(file_path)
+        self.convert_data()
+
+    def convert_data(self):
+
+        self.data = []
+        for line in self.file:
+            self.data.append(int(line.strip()))
+        print(self.data)
+
+    def get_data(self):
+        return self.data
