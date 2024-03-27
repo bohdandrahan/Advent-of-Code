08d85a586f4868e08ceffbb1c2316f36d775b3d8
bohdandrahan <bohdan.drahan@gmail.com>
2024-03-26 22:50:26 -0400

    solve 2023/13 and init 2023/14

--- 2023/13/calculator.py
+++ 2023/13/calculator.py
@@ -1,94 +1,70 @@
 
-import enum
-import sre_constants
-
 
 class Calculator():
     def __init__(self, data):
         self.data = data
+        self.rotated_data = [self.rotate_data(each) for each in data]
+        # print('START', self.rotated_data[0][0], len(
+        #     self.rotated_data), 'ROTATED <---')
+        # print('TEST', self.rotate_data(['123', '456', '789']))
 
-    def count_row(self, row):
-        print('count_row', row)
-        count = 0
+    def rotate_data(self, matrix):
+        matrix = [list(row) for row in matrix]
 
-        def dfs(string):
-            nonlocal count
+        transposed_matrix = list(zip(*matrix))
 
-            if '?' not in string:
-                record = []
-                prev = '.'
-                for char in string:
-                    if char == '#' and prev == '.':
-                        record.append(1)
-                    elif char == '#' and prev == '#':
+        rotated_matrix = ["".join(row[::-1]) for row in transposed_matrix]
 
-                        record[-1] += 1
-                    prev = char
+        return rotated_matrix
 
-                # print(string, record, row[1])
+    def calculate_box(self, box):
+        for i, line in enumerate(box):
+            # print(line)
+            # print(i)
+            possible_matches = []
+            for j, each in enumerate(box[i + 1:], i+1):
+                if each == line:
+                    if (j - i) % 2 != 1:
+                        continue
 
-                if record == row[1]:
-                    count += 1
+                    if not (i == 0 or j == len(box)-1):
+                        continue
 
-            else:
-                if not self.check_if_possible(string, row):
-                    return 0
+                    possible_matches.append(j)
 
-                i = string.index('?')
-                stirng = string[:i] + '.' + string[i+1:]
-                dfs(stirng)
+            # if (len(possible_matches)) > 1:
+                # print('MORE THAN ONE')
 
-                string = string[:i] + '#' + string[i+1:]
-                dfs(string)
+            for j in possible_matches:
+                left = i
+                right = j
+                # print('possible match', j, i)
 
-        dfs(row[0])
+                mirror_match = True
+                while right - left > 0:
+                    if box[left] != box[right]:
+                        mirror_match = False
+                    right -= 1
+                    left += 1
+                    # print('curr left', left)
 
-        return count
+                if mirror_match:
+                    return left
 
-    def check_if_possible(self, string, row):
-
-        i = string.index('?')
-
-        record = []
-        prev = '.'
-        for char in string[:i]:
-            if char == '#' and prev == '.':
-                record.append(1)
-            elif char == '#' and prev == '#':
-
-                record[-1] += 1
-            prev = char
-
-        # print(string, record, row[1])
-
-        for j, each in enumerate(record):
-
-            if j == len(record)-1:
-                break
-            if j < len(row[1]) and each != row[1][j]:
-                return False
-
-        return True
+        return 0
 
     def calculate1(self):
-        count = 0
+        result = 0
+        records = []
+        for b, box in enumerate(self.rotated_data):
+            result += self.calculate_box(box)
+            if self.calculate_box(box) != 0:
+                records.append(b)
 
-        for each in self.data:
-            count += self.count_row(each)
+        for b, box in enumerate(self.data):
+            result += 100*self.calculate_box(box)
 
-        return count
+        return result
 
     def calculate2(self):
-
-        count = 0
-
-        expanded_data = list(self.data)
-
-        for i, each in enumerate(expanded_data):
-            print('ROW', i)
-            each[0] = each[0]*5
-            each[1] = each[1]*5
-
-            count += self.count_row(each)
-
-        return count
+        pass
