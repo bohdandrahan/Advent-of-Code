63bf160559900242d16ad04318334d1fe432d469
bohdandrahan <bohdan.drahan@gmail.com>
2024-07-03 13:32:26 -0400

    solve 2018 3 part 2

--- 2018/3/calculator.py
+++ 2018/3/calculator.py
@@ -18,9 +18,26 @@ class Calculator():
     def update_field_with_next_row(self, row):
         for i in range(row['x'], row['x'] + row['width']):
             for j in range(row['y'], row['y'] + row['height']):
-                print(row[0], i, j)
 
                 self.field[i][j] += 1
+
+    def is_overlapping(self, rect1, rect2):
+
+        return not (rect1['x'] + rect1['width'] <= rect2['x'] or
+                    rect2['x'] + rect2['width'] <= rect1['x'] or
+                    rect1['y'] + rect1['height'] <= rect2['y'] or
+                    rect2['y'] + rect2['height'] <= rect1['y'])
+
+    def check_if_not_overlapping(self, row):
+
+        for i in range(len(self.df)):
+            if i+1 == int(row[0][1:]):
+                continue
+
+            if self.is_overlapping(row, self.df.iloc[i]):
+                return -1
+
+        return int(row[0][1:])
 
     def __init__(self, df):
         self.df = df
@@ -38,4 +55,5 @@ class Calculator():
         return result
 
     def calculate2(self):
-        pass
+
+        return self.df.apply(self.check_if_not_overlapping, axis=1).max()
