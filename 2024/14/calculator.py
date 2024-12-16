53535a414231081ffc5270a1cda93febd1bef806
bohdandrahan <bohdan.drahan@gmail.com>
2024-12-16 11:15:30 -0500

    solve 2024/14 part 2. It was exactly a fancy algoritm to find the min entropy or lineral algrebra, but it wasn't exactly a manual search throug white noise neither. I found a pattern that every 101 sec the pattern wasn't random so I printed every 101th element and only then I had to look at the noise which was only number 72

--- 2024/14/calculator.py
+++ 2024/14/calculator.py
@@ -1,4 +1,10 @@
+import zlib
+import struct
+import os
+
+
 class Calculator():
+
     def __init__(self, data):
         self.data = data
         self.is_test = data["is_test"]
@@ -38,9 +44,33 @@ class Calculator():
                 result *= current_num
         return result
 
+    # 7285
+
     def calculate2(self):
         print('calculate 2 is running')
-        return
+
+        k = 0
+        for i in range(50000):
+
+            self.map = [[0 for y in range(self.height)]
+                        for x in range(self.width)]
+
+            for j, robot in enumerate(self.data["robots"]):
+                next_postition = self.get_position_after_N_seconds(
+                    robot["position"], robot["velocity"], 1)
+                self.data["robots"][j]["position"] = next_postition
+                robot["position"] = next_postition
+
+                self.map[next_postition[0]][next_postition[1]] = 255
+
+            if ((i+1) - 14) % 101 == 0:
+                self.makeGrayPNG(self.map)
+                with open("2024/14/pngs/" + str(k) + "_" + str(i) + ".png", "wb") as f:
+                    f.write(self.makeGrayPNG(self.map))
+                k += 1
+
+        result = 1
+        return result
 
     def get_position_after_N_seconds(self, position, velocity, N):
         next_position = [position[0] + velocity[0], position[1] + velocity[1]]
@@ -50,3 +80,55 @@ class Calculator():
             return next_position
         else:
             return self.get_position_after_N_seconds(next_position, velocity, N - 1)
+
+    def makeGrayPNG(self, data):
+        height = self.width  # Sue me
+        width = self.height
+
+        def I1(value):
+            return struct.pack("!B", value & (2**8-1))
+
+        def I4(value):
+            return struct.pack("!I", value & (2**32-1))
+        # compute width&height from data if not explicit
+        if height is None:
+            height = len(data)  # rows
+        if width is None:
+            width = 0
+            for row in data:
+                if width < len(row):
+                    width = len(row)
+        # generate these chunks depending on image type
+        makeIHDR = True
+        makeIDAT = True
+        makeIEND = True
+        png = b"\x89" + "PNG\r\n\x1A\n".encode('ascii')
+        if makeIHDR:
+            colortype = 0  # true gray image (no palette)
+            bitdepth = 8  # with one byte per pixel (0..255)
+            compression = 0  # zlib (no choice here)
+            filtertype = 0  # adaptive (each scanline seperately)
+            interlaced = 0  # no
+            IHDR = I4(width) + I4(height) + I1(bitdepth)
+            IHDR += I1(colortype) + I1(compression)
+            IHDR += I1(filtertype) + I1(interlaced)
+            block = "IHDR".encode('ascii') + IHDR
+            png += I4(len(IHDR)) + block + I4(zlib.crc32(block))
+        if makeIDAT:
+            raw = b""
+            for y in range(height):
+                raw += b"\0"  # no filter for this scanline
+                for x in range(width):
+                    c = b"\0"  # default black pixel
+                    if y < len(data) and x < len(data[y]):
+                        c = I1(data[y][x])
+                    raw += c
+            compressor = zlib.compressobj()
+            compressed = compressor.compress(raw)
+            compressed += compressor.flush()  # !!
+            block = "IDAT".encode('ascii') + compressed
+            png += I4(len(compressed)) + block + I4(zlib.crc32(block))
+        if makeIEND:
+            block = "IEND".encode('ascii')
+            png += I4(0) + block + I4(zlib.crc32(block))
+        return png
