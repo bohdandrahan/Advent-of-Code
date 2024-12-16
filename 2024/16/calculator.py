8aed6acbc40fa90281581fa810c162eb13d34dee
bohdandrahan <bohdan.drahan@gmail.com>
2024-12-16 14:57:49 -0500

    start of the 2024 16 part 1

--- 2024/16/calculator.py
+++ 2024/16/calculator.py
@@ -1,12 +1,37 @@
-class Calculator():
+import heapq
 
+class Calculator():
     def __init__(self, data, debugger = False):
-        self.data = data
         self.debugger = debugger
 
+        self.data = data
+        # self.pr(data)
+
+        self.directions = [[0,1],[1,0],[0,-1],[-1,0]]
 
     def calculate1(self):
         print('calculate 1 is running')
+        priority_que = []
+        heapq.heapify(priority_que)
+        current_pos = self.data["start_pos"]
+        current_dir = self.directions[0] #init direction is east
+        seen = set()
+
+
+        while current_pos != self.data["end_pos"]:
+            if tuple(current_pos + current_dir) in seen:
+                continue
+            else:
+                seen.add(tuple(current_pos + current_dir))
+
+            for direction in self.directions:
+                turning_points = 0
+                if current_dir == [-direction[0], -direction[1]]: #we can't go backwards
+                    continue
+
+                #TODO: do the dijkstra's algoritm
+
+
         return 
 
     def calculate2(self):
@@ -17,3 +42,5 @@ class Calculator():
     def pr(self, content_to_print):
         if self.debugger:
             print(content_to_print)
+
+    def do_next_step(self, priority_que, 
